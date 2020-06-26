3b956f2850c194a5d411be35af053ec7c090e1c0
sandeepscet <sandeep.scet@gmail.com>
2020-06-26 19:44:39 +0530

    response send to browser

--- routes/api.js
+++ routes/api.js
@@ -6,33 +6,29 @@ var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.e
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  
+  let all = [], joined = [], profilepic = [], questions = [];
   base('Quiz').select({
     view: "Grid view",
   }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
 
     records.forEach(function (record) {
-      console.log('retrieved');
-      console.log( record.get("All"));
-      console.log( record.get("Joined"));
-      console.log( record.get("ProfilePic"));
-      console.log( record.get("Question1"));
+      all.push(record.get("All"));
+      joined.push(record.get("Joined"));
+      profilepic.push(record.get("ProfilePic"));
+      questions.push(record.get("Question1"));
     });
-
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    console.log('fetched next page');
+    
     fetchNextPage();
 
   }, function done(err) {
     if (err) { 
       console.error(err); return; 
+      res.status(500).send(err);
     }
     else{
-      console.log('done');
-      res.send({'done': true});
+      response = {all , joined , profilepic , questions };
+      res.status(200).send(response);
     }
   });
 
